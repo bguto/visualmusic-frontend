380dd612da444e0a4bd36c2e46286b29c58b06cf
bguto <guasch95@gmail.com>
2025-06-24 11:32:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = "https://visualmusic-backend.onrender.com";
+const BACKEND_URL = "https://visualmusic-backend-1.onrender.com";
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
